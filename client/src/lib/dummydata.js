1513a90f1ae5039d2280692e0624118d2cadfe3c
Yuvraj23b <yuvrajbrahmbhatt21@gnu.ac.in>
2024-06-29 19:46:42 +0530

    first commit

---
+++ client/src/lib/dummydata.js
@@ -0,0 +1,94 @@
+export const listData = [
+  {
+    id: 1,
+    title: "A Great Sofa for your Living room!",
+    images: ["https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg"],
+    price: 1000,
+    address: "456 Park Avenue, London",
+    latitude: 51.5074,
+    longitude: -0.1278,
+  },
+  {
+    id: 2,
+    title: "Feel the Luxury of Living!",
+    images: ["https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
+    price: 1500,
+    address: "789 Oxford Street, London",
+    latitude: 52.4862,
+    longitude: -1.8904,
+  },
+  {
+    id: 3,
+    title: "A New Apartment in the City!",
+    images: ["https://images.pexels.com/photos/879010/pexels-photo-879010.jpeg"],
+    bedroom: 1,
+    bathroom: 1,
+    price: 800,
+    address: "101 Baker Street, London",
+    latitude: 53.4808,
+    longitude: -2.2426,
+  },
+  {
+    id: 4,
+    title: "Book Shelf!",
+    images: ["https://images.pexels.com/photos/683929/pexels-photo-683929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
+    bedroom: 2,
+    bathroom: 1,
+    price: 1000,
+    address: "234 Kingsway, London,",
+    latitude: 53.8008,
+    longitude: -1.5491,
+  },
+  {
+    id: 5,
+    title: "Dining Tablex",
+    images: ["https://images.pexels.com/photos/269262/pexels-photo-269262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
+    bedroom: 3,
+    bathroom: 2,
+    price: 1500,
+    address: "567 Victoria Road, London",
+    latitude: 53.4084,
+    longitude: -2.9916,
+  },
+  {
+    id: 6,
+    title: "Table",
+    images: ["https://images.pexels.com/photos/2180883/pexels-photo-2180883.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
+    bedroom: 1,
+    bathroom: 1,
+    price: 800,
+    address: "890 Regent Street, London",
+    latitude: 54.9783,
+    longitude: -1.6174,
+  },
+  
+];
+
+export const singlePostData = {
+  id: 1,
+  title: "Home Decor",
+  price: 1200,
+  images: [
+   "https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg",
+   "https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/879010/pexels-photo-879010.jpeg",
+   "https://images.pexels.com/photos/683929/pexels-photo-683929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  ],
+  bedRooms: 2,
+  bathroom: 1,
+  size: 861,
+  latitude: 51.5074,
+  longitude: -0.1278,
+  city: "London",
+  address: "1234 Broadway St",
+  school: "250m away",
+  bus: "100m away",
+  restaurant: "50m away",
+  description:
+    "Future alike hill pull picture swim magic chain seed engineer nest outer raise bound easy poetry gain loud weigh me recognize farmer bare danger. actually put square leg vessels earth engine matter key cup indeed body film century shut place environment were stage vertical roof bottom lady function breeze darkness beside tin view local breathe carbon swam declared magnet escape has from pile apart route coffee storm someone hold space use ahead sheep jungle closely natural attached part top grain your grade trade corn salmon trouble new bend most teacher range anybody every seat fifteen eventually",
+};
+
+export const userData = {
+  id: 1,
+  name: "Scarlett ",
+  img: "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+};
